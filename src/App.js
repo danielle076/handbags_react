77d34f4b6270686fd6bf8c2076c3fbb8c7d26167
danielle076 <d.vandenakker@novi-education.nl>
2021-03-12 16:14:46 +0100

    stap 1 buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
 
+// Stap 1 Buttons
+// Buttons aangemaakt binnen een <div>
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  );
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav className="button">
+            <button type="button"
+                    onClick={() => console.log("Naar de collectie")}
+            >to the collection
+            </button>
+            <button type="button"
+                    onClick={() => console.log("Shop alle tassen")}
+            >shop all bags
+            </button>
+            <button
+                disabled={true}
+            >pre-orders
+            </button>
+        </nav>
+        </>
+    );
 }
 
 export default App;
-
-
-
